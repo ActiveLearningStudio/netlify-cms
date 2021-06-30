ca90626580020fbcb4523b88694ac3bc5f38875e
aqibnwaz <aquib@curriki.org>
2021-06-30 15:28:42 +0500

    updated index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,6 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { fill } from "lodash"
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-console.log(data);
-  const {allMarkdownRemark:{nodes} } = data // data.markdownRemark holds your post data
+import React from "react";
+ const  Index = () => {
+    return <h2>Hi, This is my component!</h2>;
     
-  return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-  )
-}
-export const pageQuery = graphql`
-query MyQuery {
-  allMarkdownRemark {
-    nodes {
-      html
-      frontmatter {
-        birthdate
-        color
-        country
-        description
-        featuredpost
-        member
-        title
-        your_age
-        tags
-        location
-        featuredimage
-        favouriteProjects
-        contact
-        hero
-        testonomial
-      }
-    }
   }
-}
-`
+  export default Index;
