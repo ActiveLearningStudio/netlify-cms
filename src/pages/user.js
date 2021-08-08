1f5b16d3e0d0ea80705a4c129ce9142a22c2c386
Muhammad <muhammadqamar111@gmail.com>
2021-08-08 18:47:03 +0500

    new changes

---
+++ src/pages/user.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from '../components/layout'
+import Home from '../components/Home'
+export default function Template( ) {
+ 
+  return (
+     <>
+      <Layout> 
+        <div style={{ maxWidth: '1024px', margin: '130px auto 20px'}}>
+           this is user page
+        </div>
+      </Layout>
+    </>
+  )
+}
